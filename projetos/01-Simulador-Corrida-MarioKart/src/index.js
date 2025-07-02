ae39833956d57ce5be6896910dd60cd644049d70
Caroline-A-Martins <business.carolmartins@gmail.com>
2025-07-01 23:24:52 -0300

    Declarar vencendor

--- projetos/01-Simulador-Corrida-MarioKart/src/index.js
+++ projetos/01-Simulador-Corrida-MarioKart/src/index.js
@@ -168,8 +168,6 @@ async function playRaceEngine(character1, character2) {
       console.log(`${character2.NOME} marcou um ponto!`);
       character2.PONTOS++;
     }
-    console.log(`Total de pontos ${character1.NOME}: ${character1.PONTOS}`);
-    console.log(`Total de pontos ${character2.NOME}: ${character2.PONTOS}\n`);
     console.log("------------------------------------------------");
   }
 }
@@ -179,9 +177,25 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+async function declareWinner(character1, character2) {
+	console.log("Resultado final: ")
+	console.log(`${character1.NOME}: ${character1.PONTOS} ponto(s)`)
+	console.log(`${character2.NOME}: ${character2.PONTOS} ponto(s)`)
+
+	if (character1.PONTOS > character2.PONTOS){
+		console.log(`\n ${character1.NOME} venceu a corrida.Parabéns 🏆`)
+	}else if(character2.PONTOS > character1.PONTOS){
+			console.log(`\n ${character2.NOME} venceu a corrida.Parabéns 🏆`)
+	}else{
+			console.log("A corrida terminou em empate")
+	}
+	
+}
+
 (async function main() {
   console.log(
     `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} está começando ...\n`
   );
   await playRaceEngine(player1, player2);
+	await declareWinner(player1, player2)
 })();
